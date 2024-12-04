c8e60ce1c82a3518ac9f814212724e6880f94c2d
Devi-Das-Git <71691657+Devi-Das-Git@users.noreply.github.com>
2024-12-04 20:29:20 +0530

    Update CafeQueries.cs

--- CafeAPI/CafeAPI/Application/Queries/CafeQueries.cs
+++ CafeAPI/CafeAPI/Application/Queries/CafeQueries.cs
@@ -1,4 +1,4 @@
-﻿using Cafe.Infrastructure;
+using Cafe.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic;
 
@@ -11,13 +11,11 @@ namespace CafeAPI.Application.Queries
         public async Task<IEnumerable<Cafe>> GetCafeByLocationAsync(string location)
         {
             
-            var cafes = context.Cafes.AsQueryable(); 
-            if (!string.IsNullOrEmpty(location)) { 
-                cafes = cafes.Where(c => c.Location.Equals(location, StringComparison.OrdinalIgnoreCase));
-            }
-            if (location == "default")
+            var cafes = context.Cafes.OrderByDescending(c=>c.Employees);
+            
+            if (string.IsNullOrEmpty(location) || location == "default")
             {
-                var result = await context.Cafes.OrderByDescending(c => c.Employees).Select(c => new Cafe
+                var result = await cafes.Select(c => new Cafe
                 {
                     Id = c.Id,
                     Name = c.Name,
@@ -29,6 +27,21 @@ namespace CafeAPI.Application.Queries
                 }).ToListAsync();
                 return (IEnumerable<Cafe>)result;
             }
+            if (!string.IsNullOrEmpty(location))
+            {
+                var result = await cafes.Where(x=>x.Location==location).Select(c => new Cafe
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Description = c.Description,
+                    Employees = c.Employees,
+                    Logo = c.Logo,
+                    Location = c.Location
+
+                }).ToListAsync();
+                return (IEnumerable<Cafe>)result;
+            }
+            
             IEnumerable<Cafe> emptyList = Enumerable.Empty<Cafe>();
             return (IEnumerable<Cafe>)emptyList;
             
@@ -37,8 +50,9 @@ namespace CafeAPI.Application.Queries
 
         public async Task<IEnumerable<Employee>> GetEmployeesByCafe(string cafe)
         {
-            var cafeid = new Guid(cafe);
-            var employee = context.Employees.Where(e => string.IsNullOrEmpty(cafe) || e.CafeId == cafeid);
+            //var cafeid = new Guid(cafe);
+            var cafeid = context.Cafes.Where(x => x.Name == cafe).Select(x => x.Id).FirstOrDefault();
+            var employee = context.Employees.Where(e => e.CafeId == cafeid);
 
             var employees = await employee.Select(e => new Employee
             {
